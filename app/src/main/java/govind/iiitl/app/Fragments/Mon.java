83e9fce28d8cb8ca10223ca06c829ffc01b848c8
Rohit Verma <44283521+refactor-droidyy@users.noreply.github.com>
2020-01-11 08:23:17 +0530

    finalise time-table (#77)

--- app/src/main/java/govind/iiitl/app/Fragments/Mon.java
+++ app/src/main/java/govind/iiitl/app/Fragments/Mon.java
@@ -4,59 +4,65 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 
+import govind.iiitl.app.Adapter.ListingAdapter;
+import govind.iiitl.app.Schedule;
 import govind.iiitl.app.R;
 import govind.iiitl.app.TimeTable;
 
 public class Mon extends Fragment {
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-
+    public View onCreateView(@NotNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View v = LayoutInflater.from(getContext()).inflate(R.layout.fragment_mon,null);
+        RecyclerView recyclerView = v.findViewById(R.id.rec_timetable);
         TimeTable activity = (TimeTable) getActivity();
         assert activity != null;
         String s = activity.SendData();
-        TextView txt1 = v.findViewById(R.id.textView1);
-        TextView txt2 = v.findViewById(R.id.textView2);
-        TextView txt3 = v.findViewById(R.id.textView3);
-        TextView txt4 = v.findViewById(R.id.textView4);
-        TextView txt5 = v.findViewById(R.id.textView5);
 
         try {
             JSONObject obj = new JSONObject(loadJsonFromAsset(s));
             JSONObject monday = obj.getJSONObject("Monday");
-            for(int i=0;i<monday.length();i++){
-                String morning9 = monday.getString("9:00-10:00");
-                String morning10 = monday.getString("10:00-11:00");
-                String morning12 = monday.getString("11:15-12:15");
-                String morning2 = monday.getString("12:15-1:15");
-                String morning = monday.getString("3:00-6:00");
 
-                txt1.setText(morning9);
-                txt2.setText(morning10);
-                txt3.setText(morning12);
-                txt4.setText(morning2);
-                txt5.setText(morning);
+            ArrayList<Schedule> list = new ArrayList<>();
+            Iterator keysToCopyIterator = monday.keys();
+            List<String> keysList = new ArrayList<>();
+            while(keysToCopyIterator.hasNext()) {
+                String key = (String) keysToCopyIterator.next();
+                keysList.add(key);
             }
+
+            for(int i=0;i<keysList.size();i++) {
+                String morning9 = monday.getString(keysList.get(i));
+              //  txt = txt + keysList.get(i)+ morning9 + "\n";
+                list.add(new Schedule(keysList.get(i), morning9));
+            }
+            ListingAdapter adapter = new ListingAdapter(getContext(), list);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+           recyclerView.setAdapter(adapter);
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
         return v;
-
     }
 
     private String loadJsonFromAsset(String s){

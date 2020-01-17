9691c334ae0a48962c64d9ca27b48a06d93e25f3
GOVINDDIXIT <govinddixit93@GMAIL.com>
2020-01-17 16:33:36 +0530

    add verification

---
+++ app/src/main/java/govind/iiitl/app/activities/VerifyActivity.kt
@@ -0,0 +1,35 @@
+package govind.iiitl.app.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import govind.iiitl.app.R
+import govind.iiitl.app.signIn.Login
+import kotlinx.android.synthetic.main.activity_verify.*
+
+class VerifyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify)
+
+        submitBtn.setOnClickListener {
+            submit()
+        }
+    }
+
+    private fun goToLoginActivity() {
+        startActivity(Intent(this, Login::class.java))
+        finish()
+    }
+
+    private fun submit() {
+        val text = "GODREJ"
+        if (verifyText.text.toString() == text) {
+            goToLoginActivity()
+        } else {
+            Toast.makeText(this, "Wrong Answer", Toast.LENGTH_LONG).show()
+        }
+    }
+}

f282cd46569e5244813a0d2183ab32b4b585100c
refactor-droidyy <lit2018068@iiitl.ac.in>
2020-04-13 21:23:31 +0530

    feat: Accesing past year question paper

--- app/src/main/java/govind/iiitl/app/adapter/QuestionPaperAdapter.java
+++ app/src/main/java/govind/iiitl/app/adapter/QuestionPaperAdapter.java
@@ -1,13 +1,19 @@
 package govind.iiitl.app.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.internal.InternalTokenProvider;
 
 import java.util.List;
 
@@ -34,6 +40,17 @@ public class QuestionPaperAdapter extends RecyclerView.Adapter<QuestionPaperAdap
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final QuestionPaper questionPaper = questionPaperList.get(position);
         holder.name.setText(questionPaper.name);
+        holder.pdf_click.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("IntentReset")
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(questionPaper.url));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -44,10 +61,11 @@ public class QuestionPaperAdapter extends RecyclerView.Adapter<QuestionPaperAdap
     static class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView name;
-
+        CardView pdf_click;
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.name);
+            pdf_click = itemView.findViewById(R.id.linear_question);
         }
     }
 }
